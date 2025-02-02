a8c4da52b8f93ab1756f03979ce5ae23ebd155f6
Tarun Jeevan <tjeevan200@gmail.com>
2025-02-01 18:11:18 -0600

    Save progress in case clap integration fails

--- 6-grep_cli_tool/src/main.rs
+++ 6-grep_cli_tool/src/main.rs
@@ -1,8 +1,32 @@
 use grep_cli_tool::Config;
 use std::{env, process};
 
+use clap::{ArgAction, Parser};
+
+#[derive(Parser, Debug)]
+#[command(name = "mygrep", about = "A grep-like CLI tool")]
+struct Cli {
+    /// The query string to search for
+    query: String,
+
+    /// The file to search in
+    file: String,
+
+    /// Turn on for case insensitive search
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    ignore_case: bool,
+
+    /// Turn on to show line numbers
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    line_numbers: bool,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let clap_args = Cli::parse();
+    println!("{:?}", clap_args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing the arguments: {err}");
