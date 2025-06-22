44a9b3810f682d4ff8c13207d2e8c14c6c19d9dd
Tarun Jeevan <tjeevan200@gmail.com>
2025-06-22 18:52:54 -0500

    Create simple single-threaded web server

--- 9-web_server/src/main.rs
+++ 9-web_server/src/main.rs
@@ -1,3 +1,44 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Create TCP listener bound to localhost on port 7878
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // TODO: Handle possible error case
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    // Get request line
+    let request_line = buf_reader.lines().next().unwrap().unwrap_or_else(|err| {
+        eprintln!("Error reading request line: {err}");
+        String::new()
+    });
+
+    // Construct response based on request
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap_or_else(|err| {
+        eprintln!("Error reading file to string: {err}");
+        String::new()
+    });
+    let content_len = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {content_len}\r\n\r\n{contents}");
+
+    // Send response
+    stream
+        .write_all(response.as_bytes())
+        .unwrap_or_else(|err| eprintln!("Error sending response: {err}"));
 }
